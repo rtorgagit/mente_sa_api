d3cb96b4fbbb07d639b2f2d74b3cbc98a6cab808
Roberto Torga <rtorga@gmail.com>
2022-09-06 11:49:57 -0300

    feat: add user entity

---
+++ src/database/migrations/1662474626857-CreateUser.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUser1662474626857 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          { name: 'id', type: 'uuid', isPrimary: true },
+          { name: 'nome', type: 'varchar' },
+          { name: 'email', type: 'varchar', isUnique: true },
+          { name: 'password', type: 'varchar' },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user');
+  }
+}
