166be7d608be85e973d5c26a1ae7bc3fb263e3ff
Roberto Torga <rtorga@gmail.com>
2022-09-06 20:35:29 -0300

    feat: add authentication user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    nome: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('E-mail ou senha incorretos!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('E-mail ou senha incorretos!');
+    }
+
+    const token = sign({}, 'c9809bf542d6240bea7ed1861aa38dcf7da3c1ef', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        nome: user.nome,
+        email: user.email,
+      },
+    };
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { patientsRoutes } from './patients.routes';
 import { professionalsRoutes } from './professionals.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
 
+router.use(authenticateRoutes);
 router.use('/professionals', professionalsRoutes);
 router.use('/patients', patientsRoutes);
 router.use('/users', usersRoutes);
